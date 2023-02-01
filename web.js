e5b821ba6fa7436e4422b3298b2dcf8d4b0f6dbf
leehyeongkuk <leehyeonhkuk@naver.com>
2023-02-01 17:55:26 +0900

    0201노드

---
+++ web.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+const notice = require('./api/notice');
+
+
+
+app.use(express.static( path.join(__dirname, 'public')))
+//리액트 요청주소
+app.use('/notice', notice);
+
+
+app.get('/',function(req, res){
+    res.sendFile( path.join(__dirname, 'public/index.html'))
+})
+//라우터없다면
+app.use((req, res) =>{
+    res.status(404).sendFile( path.join(__dirname, 'public/nopage.html'))
+})
+
+app.listen(PORT, () => {
+   console.log( `${PORT} 노드서버구동정상` )
+})
